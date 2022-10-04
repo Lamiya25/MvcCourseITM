0724d4722872dd22025fdbdd3100e49737db51c6
Lamiya Gafarli <81652075+Lamiya25@users.noreply.github.com>
2022-10-04 10:54:55 +0400

    Add project files.

---
+++ MvcCourseITM/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using MvcCourseITM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCourseITM.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Signin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Register(Register stud)
+        {
+            using (var db= new forgithubEntities())
+            {
+                var newstudent = db.Registers.Create();
+                newstudent.NAME = stud.NAME;
+                newstudent.SURNAME = stud.SURNAME;
+                newstudent.EMAIL = stud.EMAIL;
+                newstudent.PASSWORD = stud.PASSWORD;
+                newstudent.GENDER = stud.GENDER;
+
+                db.Registers.Add(newstudent);
+                db.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+
+            }
+
+           
+
+        }
+       
+    }
+}
